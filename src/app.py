710b55bbe9e03d5c724919bcd487d1feb78cd7c8
LarsWoutersUCLL <lars.wouters@student.ucll.be>
2026-01-17 15:35:44 +0100

    UTF 8

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/') 
+@app.route("/")
 def hello():
-    return 'Hello Cloud Native Collective'
+    return "Hello Cloud Native Collective"
+
